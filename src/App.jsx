ecb36bb3274b467cbc1489e391405148ed1d802f
SHA <33160317+shaneymar@users.noreply.github.com>
2025-09-27 20:36:49 +0530

    refractored

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,62 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
-   const [error, setError] = useState("");        
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const [userName, setUserName] = useState('');
-  const [ password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
-
     e.preventDefault();
-    setError(""); 
-    setIsSubmitted(false); 
+    setError("");
+    setIsSubmitted(false);
 
-
-    if(!userName || !password){
+    
+    if (!userName || !password) {
       setError("Both fields are required");
       return;
     }
 
-    if(userName === 'user' && password === 'password'){
-       setIsSubmitted(true);
+
+    if (userName === "user" && password === "password") {
+      setIsSubmitted(true);
+    } else {
+      setError("Invalid username or password");
     }
-    else{
-      setError('Invalid username or password')
-    }
-  }
+  };
+
   return (
     <div>
-      <h1>Login Page</h1>
       <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor='username'>UserName:</label>
-        <input type='text' 
-        value={userName}
-        id="username"
-        required
-        onChange={(e) => setUserName(e.target.value)}
-        />
-      </div>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
 
-      <div>
-        <label htmlFor='password'>Password:</label>
-        <input type='password' 
-        id="password"
-        value={password}
-        required
-        onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-      <button type='submit'>Submit</button>
+        <button type="submit">Submit</button>
       </form>
 
       
       {isSubmitted && <p>Welcome, user!</p>}
       {error && <p>{error}</p>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
